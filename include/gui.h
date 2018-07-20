8647efda5d9fd5b202c500104af1d04fc033ce51
Igor Bispo De Moraes <igorbispo@Igors-MacBook-Air.local>
2018-07-20 17:37:42 -0300

    Adicionada funcionalidade de percorrer o texto

--- include/gui.h
+++ include/gui.h
@@ -23,20 +23,36 @@ typedef struct text_slice {
 } text_slice;
 
 // Class gui
-#define gui_ {&_run, &_move_cursor, &_display_txt, &_clean}
+#define gui_ {&_run, &_can_move_cursor,\
+&_move_cursor, &_display_txt, &_clean}
 
 typedef struct gui gui;
 
 struct gui {
   int (*run) (file*);
+  bool (*can_move_cursor) (int, text*, text_slice);
   int (*move_cursor) (int);
   int (*display_txt) (text*, text_slice);
   int (*clean) (void);
 };
 
 int _run(file*);
+bool _can_move_cursor(int, text*, text_slice);
 int _move_cursor(int);
 int _display_txt(text*, text_slice);
 int _clean (void);
+// End of class gui
+
+// Misc functions
+bool is_arrow(int);
+
+// Operator functions
+typedef void (*op_func) (int* x, int* y);
+void _op_up(int* x, int* y);
+void _op_down(int* x, int* y);
+void _op_right(int* x, int* y);
+void _op_left(int* x, int* y);
+op_func eval_arrow(int);
+
 
 #endif
--- src/gui.c
+++ src/gui.c
@@ -1,27 +1,89 @@
 #include "gui.h"
 
+// OP functions
+void _op_up(int* x, int* y) {*x=*x; *y=*y-1;}
+void _op_down(int* x, int* y) {*x=*x; *y=*y+1;}
+void _op_right(int* x, int* y) {*x=*x+1; *y=*y;}
+void _op_left(int* x, int* y) {*x=*x-1; *y=*y;}
+
+op_func eval_arrow(int k) {
+  switch (k) {
+    case KEY_UP:
+      return _op_up;
+      break;
+    case KEY_DOWN:
+      return _op_down;
+      break;
+    case KEY_LEFT:
+      return _op_left;
+      break;
+    case KEY_RIGHT:
+      return _op_right;
+      break;
+    default:
+      return NULL;
+  }
+}
+
+bool _can_move_cursor (int k, text* txt,
+    text_slice txt_slc) {
+  int cursor_x, next_x;
+  int cursor_y, next_y;
+
+  // Getting current cursor position
+  getyx(stdscr, cursor_y, cursor_x); 
+  next_x = cursor_x;
+  next_y = cursor_y;
+
+  //Getting current screen size
+  int screen_x, screen_y;
+  getmaxyx(stdscr, screen_y, screen_x);
+
+  // Evaluating operator function
+  op_func op = eval_arrow(k);
+  op(&next_x, &next_y);
+
+  /* --------- Testing y_coord --------- */
+  //Out of screen frame
+  if (next_y < 1 || next_y > screen_y - 1) {
+    return false;
+  }
+
+  //Out of txt bounds
+  if (next_y > txt_slc.to_y + 1) {
+    return false;
+  }
+
+  /* --------- Testing x_coord --------- */
+  //Out of screen frame
+  if (next_x < 1 || next_x > screen_x - 1) {
+    return false;
+  }
+
+  //Out of txt bounds
+  if (next_x > strlen(txt->lines[next_y-1]) + 1) {
+    return false;
+  }
+
+  return true;
+}
+
 int _move_cursor (int k) {
   int cursor_x;
   int cursor_y;
-  int return_value;
-  getyx(stdscr, cursor_y, cursor_x);
 
-  switch (k) {
-    case KEY_UP:
-      move(cursor_y - 1, cursor_x);
-      break;
-    case KEY_DOWN:
-      move(cursor_y + 1, cursor_x);
-      break;
-    case KEY_LEFT:
-      move(cursor_y, cursor_x - 1);
-      break;
-    case KEY_RIGHT:
-      move(cursor_y, cursor_x + 1);
-      break;
+  getyx(stdscr, cursor_y, cursor_x);
+  op_func op = eval_arrow(k);   
+
+  if (op) {
+    op(&cursor_x, &cursor_y);
+    move(cursor_y, cursor_x);
+    refresh();
+
+    return SUCCESS;   
+  } else {
+    return ERROR;
   }
-  refresh();
-  return SUCCESS;
 }
 
 int _display_txt (text* txt, text_slice txt_slc) {
@@ -65,7 +127,7 @@ int _run (file* f) {
 
   // Init routines
   initscr();
-  noecho();
+  //noecho();
   nonl();
   keypad(stdscr, TRUE);
   getmaxyx(stdscr, size_y, size_x);
@@ -83,10 +145,14 @@ int _run (file* f) {
 
   while (1) {
     k = getch();
+
+    // TODO Refactorize GUI execution flow
     if (k == KEY_END) {
       insert_mode = !insert_mode;
     } else if (insert_mode) {
-      _move_cursor(k);
+      if (is_arrow(k))
+        if (_can_move_cursor(k, f->txt, txt_slc))
+          _move_cursor(k);
     } else if (k == 'q') {
       break;
     }
@@ -100,3 +166,16 @@ int _clean () {
   endwin();
   return SUCCESS;
 }
+
+// End of class gui
+
+// Misc functions
+bool is_arrow(int k) {
+  int arrow_types[] = {KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT};
+  size_t num_of_arr = 4;
+  for (size_t i = 0;i < num_of_arr;i++) {
+    if (k == arrow_types[i])
+      return true;
+  }
+  return false;
+} 
